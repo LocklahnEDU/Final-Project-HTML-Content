f828744897cbb35950325019bd47de889f29e15a
LocklahnEDU <locklahnfrancis@mail.weber.edu>
2024-12-04 23:09:43 -0700

    V4 Final

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 function CalculateKennel(){
+  
   let dogSize = document.getElementById("dogSize").value;
   let numDays = document.getElementById("numDays").value;
+  if (numDays == 0) {
+    alert("Please enter an amount of time.")
+  }
   let beforeTax = parseInt(dogSize) * numDays;
   let totalCost = Math.round(100 * (beforeTax + (beforeTax * 0.0485))) / 100;
   document.getElementById("answer").textContent = `Cost: $${totalCost}`;
